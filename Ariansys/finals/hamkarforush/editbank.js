a319a3904206a0752cfc3b82d16c3f056aee330d
ariansystemdp\m.baharlou <looli1995@gmail.com>
2025-09-06 10:16:09 +0330

    hamkarforush

---
+++ Ariansys/finals/hamkarforush/editbank.js
@@ -0,0 +1,111 @@
+const { Builder, By, until, Key, Actions } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+const fs = require("fs");
+const path = require("path");
+const customDriver = require("../../customerDriver");
+
+const colors = {
+  red: "\x1b[31m",
+  green: "\x1b[32m",
+  reset: "\x1b[0m",
+};
+
+async function editbank() {
+  // تولید کد ملی با متد customerDriver
+  const nationalId = customDriver.generateNationalId();
+  console.log("کد ملی تولید شده:", nationalId);
+
+  // ساخت درایور با اکتیو بودن نوتیفیکیشن و ری‌استور persist
+  let dr = new customDriver();
+  const url = "https://frontbuild.ariansystemdp.local/fa";
+  let driver = await dr.createDriver(url, true);
+
+  try {
+    // لاگین با متد customerDriver
+    await dr.login();
+
+    // اجرای گام‌ها
+    const steps = [
+      "/html/body/div[3]/div/div[2]/div[1]/div[2]/div/div[3]/div/ul/li[1]",
+      "/html/body/div[3]/div/div[2]/div[1]/div[2]/div/div[3]/div/ul/li[1]/ul/li[1]",
+      "/html/body/div[3]/div/div[2]/div[1]/div[2]/div/div[3]/div/ul/li[1]/ul/li[1]/ul/li[7]",
+      "/html/body/div[3]/div/div[2]/div[2]/div[2]/div/div[2]/div[1]/div/div/div/div/div/div/table/tbody/tr[1]/td[4]/div/span[1]",
+    ];
+
+    for (const xpath of steps) {
+      await driver.findElement(By.xpath(xpath)).click();
+      await driver.sleep(100);
+    }
+    
+    await driver
+      .findElement(
+        By.xpath(
+          "/html/body/div[3]/div/div[2]/div[2]/div[2]/div/div[2]/form/div[1]/div/div[2]/div[1]/div/input"
+        )
+      )
+      .sendKeys(Key.CONTROL + "a");
+    await driver.sleep(100);
+    await driver
+      .findElement(
+        By.xpath(
+          "/html/body/div[3]/div/div[2]/div[2]/div[2]/div/div[2]/form/div[1]/div/div[2]/div[1]/div/input"
+        )
+      )
+      .sendKeys(Key.DELETE);
+    await driver.sleep(100);
+    await driver
+      .findElement(
+        By.xpath(
+          "/html/body/div[3]/div/div[2]/div[2]/div[2]/div/div[2]/form/div[1]/div/div[2]/div[1]/div/input"
+        )
+      )
+      .sendKeys(nationalId);
+    await driver.sleep(100);
+    await driver
+      .findElement(
+        By.xpath(
+          "/html/body/div[3]/div/div[2]/div[2]/div[2]/div/div[2]/form/div[2]/div/div[2]/div/div/input"
+        )
+      )
+      .sendKeys(Key.CONTROL + "a");
+    await driver.sleep(100);
+    await driver
+      .findElement(
+        By.xpath(
+          "/html/body/div[3]/div/div[2]/div[2]/div[2]/div/div[2]/form/div[2]/div/div[2]/div/div/input"
+        )
+      )
+      .sendKeys(Key.DELETE);
+    await driver.sleep(100);
+    await driver
+      .findElement(
+        By.xpath(
+          "/html/body/div[3]/div/div[2]/div[2]/div[2]/div/div[2]/form/div[2]/div/div[2]/div/div/input"
+        )
+      )
+      .sendKeys(nationalId);
+    await driver.sleep(100);
+    await driver
+      .findElement(
+        By.xpath(
+          "/html/body/div[3]/div/div[2]/div[2]/div[2]/div/div[1]/div/div[2]/div"
+        )
+      )
+      .click();
+    await driver.sleep(100);
+
+    let bodyText = await driver.findElement(By.css("body")).getText();
+    if (bodyText.includes("تنظیم شده")) {
+      console.log(`${colors.green}ok Aryan ${colors.reset}`);
+    } else {
+      console.log(`${colors.red}not ok Aryan ${colors.reset}`);
+    }
+  } catch (err) {
+    console.error("❌ خطا:", err);
+  } finally {
+    await driver.quit();
+  }
+}
+
+// editbank();
+module.exports = editbank;
---
+++ Ariansys/finals/hamkarforush/runner8.js
@@ -0,0 +1,49 @@
+const { Builder, By, until } = require("selenium-webdriver");
+
+async function runScriptsSequentially() {
+    let driver;
+    try {
+        // مرورگر با تنظیمات نوتیفیکیشن Allow
+        const chrome = require("selenium-webdriver/chrome");
+        const options = new chrome.Options();
+        options.addArguments("--disable-popup-blocking");
+        options.setUserPreferences({
+            "profile.default_content_setting_values.notifications": 1
+        });
+
+        driver = await new Builder()
+            .forBrowser("chrome")
+            .setChromeOptions(options)
+            .build();
+
+        // اجرای اسکریپت‌ها به ترتیب و با driver مشترک
+        // console.log("--- Running phones.js ---");
+        // const phones = require("./phones");
+        // await phones(driver);
+
+        console.log("--- Running bank.js ---");
+        const bank = require("./bank");
+        await bank(driver);
+
+        console.log("--- Running editmarkaz.js ---");
+        const editbank = require("./editbank");
+        await editbank(driver);
+
+        console.log("--- Running activebank.js ---");
+        const activebank = require("./activebank");
+        await activebank(driver);
+
+        console.log("--- Running deletebank.js ---");
+        const deletebank = require("./deletebank");
+        await deletebank(driver);
+
+        
+
+    } catch (err) {
+        console.error("Error in scripts:", err);
+    } finally {
+        console.log("Closing browser...");
+        if (driver) await driver.quit();
+    }
+}
+runScriptsSequentially();
